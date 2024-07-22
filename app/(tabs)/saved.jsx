912107eda7f5123224094920f1decac1cd30e401
Nachete <ignaciocabrerahdez97@gmail.com>
2024-07-22 10:43:36 +0100

    first commit

---
+++ app/(tabs)/saved.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FlatList, Image, RefreshControl, Text, View } from "react-native";
+import useAppwrite from "../../lib/useAppwrite";
+import { getLatestPosts } from "../../lib/appwrite";
+import { EmptyState, SearchInput, Trending, VideoCard } from "../../components";
+
+const Saved = () => {
+  const { data: latestPosts, refetch } = useAppwrite(getLatestPosts);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
+  return (
+    <SafeAreaView className="bg-primary">
+      <FlatList
+        data={latestPosts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard
+            title={item.title}
+            thumbnail={item.thumbnail}
+            video={item.video}
+            creator={item.creator.username}
+            avatar={item.creator.avatar}
+          />
+        )}
+        ListHeaderComponent={() => (
+          <View className="flex my-6 px-4 space-y-6">
+            <View className="flex justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="text-2xl text-white font-psemibold">
+                  Saved Videos
+                </Text>
+              </View>
+            </View>
+
+            <SearchInput />
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos created yet"
+          />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Saved;
